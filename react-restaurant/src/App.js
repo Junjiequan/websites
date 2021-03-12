10b633857a2c6eaaa17f09e580222b30e8df0825
mmmG <a331998513@gmail.com>
2021-03-12 09:58:55 +0100

    setup

--- react-restaurant/src/App.js
+++ react-restaurant/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar/index'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and safe to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React why the fuck it doesnt change
-        </a>
-      </header>
+      <h1><Navbar /></h1>
     </div>
   );
 }
