0e744b3d486a0e8a038a44ed7c0ed9bdd0db1583
mmmG <a331998513@gmail.com>
2021-03-15 02:51:48 +0100

    sushi

--- react-restaurant/src/components/Hero/index.js
+++ react-restaurant/src/components/Hero/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../Navbar'
-import {HeroContainer, HeroContent, HeroItems, HeroH1, HeroP, HeroBtn} from './HeroElements'
+import {HeroContainer, HeroContent, HeroItems, HeroH1, HeroP, HeroBtn, HeroSpan} from './HeroElements'
 
 const Hero = () => {
     return (
@@ -8,7 +8,7 @@ const Hero = () => {
             <Navbar />
             <HeroContent>
                 <HeroItems>
-                    <HeroH1>Greatest Sushi Ever</HeroH1>
+                    <HeroH1>Greatest <HeroSpan>Sushi</HeroSpan> Ever</HeroH1>
                     <HeroP>Ready in 60 seconds</HeroP>
                     <HeroBtn>Place Order</HeroBtn>
                 </HeroItems>
---
+++ react-restaurant/src/components/Sidebar/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { 
+    SidebarContainer,
+    Icon,
+    CloseIcon,
+    SidebarMenu,
+    SidebarLink,
+    SideBtnWrap,
+    SidebarRoute 
+} from './SidebarElements'
+
+const Sidebar = () => {
+    return (
+        <SidebarContainer>
+            <Icon>
+                <CloseIcon />
+            </Icon>
+            <SidebarMenu>
+                <SidebarLink to="/">Sushi roll</SidebarLink>
+                <SidebarLink to="/">Sushi Nigiri</SidebarLink>
+                <SidebarLink to="/">Full Menu</SidebarLink>
+            </SidebarMenu>
+            <SideBtnWrap>
+                <SidebarRoute to="/">Order Now</SidebarRoute>
+            </SideBtnWrap>
+        </SidebarContainer>
+    )
+}
+
+export default Sidebar
